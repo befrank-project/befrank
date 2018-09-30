0b7bdd25fc47c71a6f1afc19e949e77a4f269271
Damien <dtrog@me.com>
2018-09-30 15:31:39 +0200

    Corrected version for release beta Fixed view-key and mnemonic rpc call

--- src/Wallet/WalletRpcServer.cpp
+++ src/Wallet/WalletRpcServer.cpp
@@ -346,11 +346,18 @@ bool wallet_rpc_server::on_get_address(const wallet_rpc::COMMAND_RPC_GET_ADDRESS
 bool wallet_rpc_server::on_query_key(const wallet_rpc::COMMAND_RPC_QUERY_KEY::request& req,
 	wallet_rpc::COMMAND_RPC_QUERY_KEY::response& res)
 {
-	if (0 != req.key_type.compare("mnemonic"))
-		throw JsonRpc::JsonRpcError(WALLET_RPC_ERROR_CODE_UNKNOWN_ERROR, std::string("Unsupported key_type ") + req.key_type);
-
-	if (!m_wallet.getSeed(res.key))
-		throw JsonRpc::JsonRpcError(WALLET_RPC_ERROR_CODE_UNKNOWN_ERROR, std::string("The wallet is non-deterministic. Cannot display seed."));
+	if (req.key_type.compare("mnemonic")) {
+		if (!m_wallet.getSeed(res.key))
+			throw JsonRpc::JsonRpcError(WALLET_RPC_ERROR_CODE_UNKNOWN_ERROR,
+										std::string("The wallet is non-deterministic. Cannot display seed."));
+	} else if (req.key_type.compare("view-key")) {
+		AccountKeys keys;
+		m_wallet.getAccountKeys(keys);
+		res.key =  Common::podToHex(keys.viewSecretKey);
+	} else {
+		throw JsonRpc::JsonRpcError(WALLET_RPC_ERROR_CODE_UNKNOWN_ERROR,
+									std::string("Unsupported key_type ") + req.key_type);
+	}
 	return true;
 }
 
